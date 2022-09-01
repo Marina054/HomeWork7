0da7d49581c65f3a94c18a27177a22f7977e4098
marina <litvinova_06@bk.ru>
2022-09-02 00:58:49 +0500

    work1

--- work_1/Program.cs
+++ work_1/Program.cs
@@ -10,22 +10,19 @@
 
 
 
-internal class Program
-{
-    private static void Main(string[] args)
+
+using System;
+
+int [,] array = new int[3,4];
+for (int i = 0; i < array.GetLength(0); i++)
     {
-        
-        double[,] array = 
-    {
-        {0.5, 7, -2, -0.2},
-        {1, -3.3, 8,  -9.9},
-        {8, 7.8, -7,  19  },
-    };
-        for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                    Console.Write(array[i, j] + "   ");
-                Console.WriteLine();
-            }
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(-10,10);
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
     }
-}
+
+
+
